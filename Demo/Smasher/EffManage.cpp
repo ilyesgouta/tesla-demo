ecf80dde584b05da30bccf0a42b8b056d35c91ff
Ilyes Gouta <ilyes.gouta@gmail.com>
2011-08-14 17:03:32 +0100

    effmange: update formatting

--- Demo/Smasher/EffManage.cpp
+++ Demo/Smasher/EffManage.cpp
@@ -1,47 +1,40 @@
 
-
+#ifdef WIN32
 #include "stdafx.h"
+#endif
+
+#include <string.h>
+
 #include "EffManage.hpp"
 
-
-
-
-/*******************************************************************************************/
-CEffManager::CEffManager() {
-
-        memset( m_aEffects, 0, sizeof(m_aEffects) );
-        m_iEffects = 0;
+CEffManager::CEffManager()
+{
+    memset( m_aEffects, 0, sizeof(m_aEffects) );
+    m_iEffects = 0;
 }
-/*******************************************************************************************/
-CEffManager::~CEffManager() {
 
-        for ( int i = 0; i != m_iEffects; i++ )
-        {
-          delete m_aEffects[i];
-        }
+CEffManager::~CEffManager()
+{
+    for ( int i = 0; i != m_iEffects; i++ )
+        delete m_aEffects[i];
 }
-/*******************************************************************************************/
-bool CEffManager::AddEffect( CEffect* pEffect, float fTimeStart, float fTimeEnd ) {
 
-        if ( m_iEffects >= EFFECTS_MAX ) return false;
+bool CEffManager::AddEffect( CEffect* pEffect, float fTimeStart, float fTimeEnd )
+{
+    if ( m_iEffects >= EFFECTS_MAX )
+        return false;
 
-        m_aEffects[m_iEffects] = pEffect;
-        m_aTStart[m_iEffects] = fTimeStart;
-        m_aTEnd[m_iEffects] = fTimeEnd;
+    m_aEffects[m_iEffects] = pEffect;
+    m_aTStart[m_iEffects] = fTimeStart;
+    m_aTEnd[m_iEffects] = fTimeEnd;
 
-        m_iEffects++;
-        return true;
+    m_iEffects++;
+    return true;
 }        
-/*******************************************************************************************/
-void CEffManager::PlayEffects( float fTime ) {
 
-        for ( int i = 0; i != m_iEffects; i++ )
-        {
-          if ( fTime > m_aTStart[i] && fTime < m_aTEnd[i] )
-          {
+void CEffManager::PlayEffects( float fTime )
+{
+    for ( int i = 0; i != m_iEffects; i++ )
+        if ( fTime > m_aTStart[i] && fTime < m_aTEnd[i] )
             m_aEffects[i]->Do( fTime, m_aTStart[i] );
-          }
-        }
 }
-/*******************************************************************************************/
-
--- Demo/Smasher/EffManage.hpp
+++ Demo/Smasher/EffManage.hpp
@@ -4,28 +4,23 @@
 
 #include "Effect.hpp"
 
-
 #define EFFECTS_MAX 64
 
 class CEffManager {
 
 public:
-        CEffManager();
-        virtual ~CEffManager();
+    CEffManager();
+    virtual ~CEffManager();
 
-        bool AddEffect( CEffect* pEffect, float fTimeStart, float fTimeEnd );
+    bool AddEffect( CEffect* pEffect, float fTimeStart, float fTimeEnd );
 
-        void PlayEffects( float fTime );
-
+    void PlayEffects( float fTime );
 
 protected:
-
-        CEffect* m_aEffects[EFFECTS_MAX];
-        float m_aTStart[EFFECTS_MAX];
-        float m_aTEnd[EFFECTS_MAX];
-        int m_iEffects;
+    CEffect* m_aEffects[EFFECTS_MAX];
+    float m_aTStart[EFFECTS_MAX];
+    float m_aTEnd[EFFECTS_MAX];
+    int m_iEffects;
 };
-
-
 
 #endif /*_EFF_MANAGE_HPP_*/
