dc1aab8086345ca38a0067751b065ed135187955
Ilyes Gouta <ilyes.gouta@gmail.com>
2011-08-30 23:21:05 +0100

    scape: code cleanup

--- Demo/Smasher/TerrainGen.cpp
+++ Demo/Smasher/TerrainGen.cpp
@@ -1,129 +1,137 @@
 
+#ifdef WIN32
 #include "stdafx.h"
+#endif
+
 #include "TerrainGen.hpp"
 
-/***********************************************************************************/
+#include <stdio.h>
 
+CTerrain::CTerrain()
+{
+    m_pGridTable = 0;
 
+    m_dwResX = m_dwResZ = 0;
 
-CTerrain::CTerrain() {
-
-        m_pGridTable = 0;
-  
-        m_dwResX = m_dwResZ = 0;
-
-        m_pMapVertices = 0;
-        m_nMapVertices = 0;
-
+    m_pMapVertices = 0;
+    m_nMapVertices = 0;
 };
-/***********************************************************************************/
-CTerrain::~CTerrain() {
 
-        if ( m_pMapVertices ) delete[] m_pMapVertices;
-        if ( m_pGridTable ) delete m_pGridTable;
+CTerrain::~CTerrain()
+{
+    if ( m_pMapVertices )
+        delete[] m_pMapVertices;
+    if ( m_pGridTable )
+        delete m_pGridTable;
 }
-/***********************************************************************************/
-bool CTerrain::LoadRawMap( char* pszMapFileName ) {
 
-        unsigned int dwFileSize;
-  
-        if ( !pszMapFileName ) return false;
-        if ( *pszMapFileName == 0 ) return false;
+bool CTerrain::LoadRawMap( char* pszMapFileName )
+{
+    unsigned int dwFileSize;
 
-        FILE* pFile = fopen( pszMapFileName, "rb" );
+    if ( !pszMapFileName )
+        return false;
 
-        if ( !pFile ) return false;
-        
-        fseek( pFile, 0, SEEK_END );
-        dwFileSize = ftell( pFile );
-        fseek( pFile, 0, SEEK_SET );
+    if ( *pszMapFileName == 0 )
+        return false;
 
-        /* temp: assume map res is 512x512 */
-        if ( dwFileSize != 512*512 ) return false;
+    FILE* pFile = fopen( pszMapFileName, "rb" );
 
-        m_dwResX = 512;
-        m_dwResZ = 512;
+    if ( !pFile )
+        return false;
 
-        unsigned char* pMapRawData = new unsigned char[dwFileSize];
-        memset( pMapRawData, 0, dwFileSize );
+    fseek( pFile, 0, SEEK_END );
+    dwFileSize = ftell( pFile );
+    fseek( pFile, 0, SEEK_SET );
 
-        if ( !pMapRawData ) 
-        {
-          fclose( pFile );
-          return false;
-        }
+    /* temp: assume map res is 512x512 */
+    if ( dwFileSize != 512 * 512 )
+        return false;
 
-        if ( fread(pMapRawData, dwFileSize, 1, pFile) != 1 )
-        {
-          delete[] pMapRawData;
-          fclose( pFile );
-          return false;
-        }
+    m_dwResX = 512;
+    m_dwResZ = 512;
 
+    unsigned char* pMapRawData = new unsigned char[dwFileSize];
+    memset( pMapRawData, 0, dwFileSize );
+
+    if ( !pMapRawData )
+    {
         fclose( pFile );
+        return false;
+    }
 
-        GenMeshFromRaw( pMapRawData );
-
+    if ( fread(pMapRawData, dwFileSize, 1, pFile) != 1 )
+    {
         delete[] pMapRawData;
+        fclose( pFile );
+        return false;
+    }
 
-        m_pGridTable->SetTexture( g_cTexManager.LoadTexture("data/textures/scape.jpg", 1), 0 );
+    fclose( pFile );
 
-        return true;
+    GenMeshFromRaw( pMapRawData );
+
+    delete[] pMapRawData;
+
+    //m_pGridTable->SetTexture( g_cTexManager.LoadTexture("data/textures/scape.jpg", 1), 0 );
+    m_pGridTable->SetTexture( g_cTexManager.LoadTexture("data/textures/gothickiemura02.jpg", 0), 0 );
+
+    return true;
 }
-/***********************************************************************************/
+
 static const float l_fMeshMinY =  0.f;
 static const float l_fMeshMaxY = 300.f;
 static const float l_fMapGridSize = 8.f;
-/***********************************************************************************/
-void CTerrain::GenMeshFromRaw( unsigned char* pMapRawData ) {
 
-        unsigned int x, z;
-        float fMeshDelY = l_fMeshMaxY - l_fMeshMinY;
-        
-        float fMapX, fMapY, fMapZ;
+void CTerrain::GenMeshFromRaw( unsigned char* pMapRawData )
+{
+    unsigned int x, z;
+    float fMeshDelY = l_fMeshMaxY - l_fMeshMinY;
 
-        if ( m_pMapVertices ) delete[] m_pMapVertices;
+    float fMapX, fMapY, fMapZ;
 
-        m_pMapVertices = new CVector[m_dwResX*m_dwResZ];
+    if ( m_pMapVertices )
+        delete[] m_pMapVertices;
 
-        for ( z = 0; z != m_dwResZ; z++ )
+    m_pMapVertices = new CVector[m_dwResX * m_dwResZ];
+
+    for ( z = 0; z != m_dwResZ; z++ )
+    {
+        for ( x = 0; x != m_dwResX; x++ )
         {
-          for ( x = 0; x != m_dwResX; x++ )
-          {
-            fMapX = (float)x*l_fMapGridSize;
-            fMapY = (float)pMapRawData[x + z*m_dwResX]/256.f*fMeshDelY + l_fMeshMinY;
-            fMapZ = (float)z*l_fMapGridSize;
-            
-            m_pMapVertices[x + z*m_dwResX] = CVector(fMapX, fMapY, fMapZ);
+            fMapX = (float)x * l_fMapGridSize;
+            fMapY = (float)pMapRawData[x + z * m_dwResX] / 256.f * fMeshDelY + l_fMeshMinY;
+            fMapZ = (float)z * l_fMapGridSize;
+
+            m_pMapVertices[x + z * m_dwResX] = CVector(fMapX, fMapY, fMapZ);
 
             m_nMapVertices++;
-          }
         }
+    }
 
-        InitGrid();
+    InitGrid();
 }
-/***********************************************************************************/
-void CTerrain::InitGrid() {
-  
-        m_pGridTable = new CGridTable;
 
-        m_pGridTable->Init( 8, 8, l_fMapGridSize, m_pMapVertices, m_dwResX, m_dwResZ );
+void CTerrain::InitGrid()
+{
+    m_pGridTable = new CGridTable;
+
+    m_pGridTable->Init( 8, 8, l_fMapGridSize, m_pMapVertices, m_dwResX, m_dwResZ );
 }
-/***********************************************************************************/
-void CTerrain::Render( CCamera& cCamera ) {
 
-        CQuaternion cBase;
+void CTerrain::Render( CCamera& cCamera )
+{
+    CQuaternion cBase;
 
-        glMatrixMode( GL_PROJECTION );
-        glLoadMatrixf( cCamera.GetProjectionMatrix()->aMatrix );
+    glMatrixMode( GL_PROJECTION );
+    glLoadMatrixf( cCamera.GetProjectionMatrix()->m_.aMatrix );
 
-        glMatrixMode( GL_MODELVIEW );
-        glLoadMatrixf( cCamera.GetCameraMatrix()->aMatrix );
+    glMatrixMode( GL_MODELVIEW );
+    glLoadMatrixf( cCamera.GetCameraMatrix()->m_.aMatrix );
 
-        glEnable( GL_TEXTURE_2D );
-        glEnable( GL_CULL_FACE );
-        glEnable( GL_DEPTH_TEST );
+    glEnable( GL_TEXTURE_2D );
+    glEnable( GL_CULL_FACE );
+    glEnable( GL_DEPTH_TEST );
 
-        m_pGridTable->Render( cCamera );
+    m_pGridTable->Render( cCamera );
 }
-/***********************************************************************************/
--- Demo/Smasher/TerrainGen.hpp
+++ Demo/Smasher/TerrainGen.hpp
@@ -5,40 +5,44 @@
 
 #include "GridNode.hpp"
 
+#include "base3d/Camera.hpp"
+#include "base3d/CMatrix.hpp"
+#include "base3d/Quaternion.hpp"
+
+#include "TexManager.hpp"
+
 class CTerrain {
   
 public:
-        CTerrain();
-        ~CTerrain();
+    CTerrain();
+    ~CTerrain();
 
-        /* output: ture if success, false if fail */
-        bool LoadRawMap( char* pszMapFileName );
+    /* output: ture if success, false if fail */
+    bool LoadRawMap( char* pszMapFileName );
 
-        void Render( CCamera& cCamera );
-        void Collision();
+    void Render( CCamera& cCamera );
+    void Collision();
 
-        float GetSample( CVector* pNormal, const float fX, const float fZ ) {
-          return m_pGridTable->GetSample( pNormal, fX, fZ );
-        }
+    float GetSample( CVector* pNormal, const float fX, const float fZ ) {
+        return m_pGridTable->GetSample( pNormal, fX, fZ );
+    }
 
 protected:
+    /* map resolution */
+    unsigned int m_dwResX, m_dwResZ;
 
-        /* map resolution */
-        unsigned int m_dwResX, m_dwResZ;
+    /* mesh data */
+    CVector* m_pMapVertices;
+    int m_nMapVertices;
 
-        /* mesh data */
-        CVector* m_pMapVertices;
-        int m_nMapVertices;
+    /* generates mesh from raw file */
+    void GenMeshFromRaw( unsigned char* pMapRawData );
 
-        /* generates mesh from raw file */
-        void GenMeshFromRaw( unsigned char* pMapRawData );
+    /* */
+    void InitGrid();
 
-        /* */
-        void InitGrid();
-
-        /* grid tables */
-        CGridTable* m_pGridTable;
-
+    /* grid tables */
+    CGridTable* m_pGridTable;
 };
 
 #endif /* _CTerrain_hpp_ */
